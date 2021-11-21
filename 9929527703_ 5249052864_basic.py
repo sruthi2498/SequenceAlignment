e83aa68337849fec23635099d248d2a65449e868
Sruthi V <sruthiv@Sruthis-MacBook-Pro.local>
2021-11-21 13:44:29 -0800

    basic

---
+++ 9929527703_ 5249052864_basic.py
@@ -0,0 +1,8 @@
+# gap penalty
+# mismatch penalty
+# input generator : read from input.txt
+# sequence alignment
+# output 
+    # The first 50 elements and the last 50 elements of the actual alignment.
+    # 2. The time it took to complete the entire solution.
+    # 3. Memory required.
